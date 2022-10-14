eb9eb0b5a2e02728c46f20117b14e2d1d4c4d12f
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2022-10-14 21:52:36 +0200

    Initial Commit

---
+++ BackgroundAnimation/BackgroundAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundAnimationApp.swift
+//  BackgroundAnimation
+//
+//  Created by Tommy Ludwig on 14.10.22.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackgroundAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BackgroundAnimation
+//
+//  Created by Tommy Ludwig on 14.10.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
